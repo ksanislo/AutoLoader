e1095ce16b17b7611aba0f9d5b95278ff699ac81
Ken Sanislo <ken@intherack.com>
2016-02-22 21:40:08 -0800

    url for v0.5.0

--- source/main.cpp
+++ source/main.cpp
@@ -121,7 +121,7 @@ int main(int argc, char **argv)
 	app::App app;
 
 	//Change this to your own URL.
-	char *url = (char*)"http://3ds.intherack.com/QRWebLoader.cia";
+	char *url = (char*)"https://github.com/ksanislo/QRWebLoader/releases/download/v0.5.0/QRWebLoader.cia";
 
 	printf("Downloading %s\n",url);
 	gpu::flushBuffer();
