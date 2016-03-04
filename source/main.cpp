aa01a2d921b87d5f42533f5722bc4a3fb896822d
Ken Sanislo <ken@intherack.com>
2016-03-03 22:20:53 -0800

    better error handling

--- source/main.cpp
+++ source/main.cpp
@@ -13,129 +13,174 @@
 // If we don't have a file, we'll use this one.
 #define DEFAULT_URL "http://3ds.intherack.com/files/web-updater_autoinstall.cia"
 
+#define SSLOPTION_NOVERIFY 1<<9
+
 // Max URL length used in qr variation
 #define QUIRC_MAX_PAYLOAD	8896
 
 
 using namespace ctr;
 
-bool onProgress(u64 pos, u64 size) {
-        printf("pos: %" PRId64 "-%" PRId64 "\n", pos, size);
-        gpu::flushBuffer();
-        hid::poll();
-        return !hid::pressed(hid::BUTTON_B);
+bool onProgress(u64 pos, u64 size){
+	printf("pos: %" PRId64 "-%" PRId64 "\n", pos, size);
+	gpu::flushBuffer();
+	hid::poll();
+	return !hid::pressed(hid::BUTTON_B);
 }
 
-Result http_getinfo(char *url, app::App *app) {
+Result http_getinfo(char *url, app::App *app){
 	Result ret=0;
+	u8* buf;
 	u32 statuscode=0;
 	httpcContext context;
 
-        app->mediaType = fs::SD;
+	app->mediaType = fs::SD;
 	app->size = 0;
 	app->titleId = 0x0000000000000000;
 
 	ret = httpcOpenContext(&context, HTTPC_METHOD_GET, url, 0);
-        if(ret!=0)return ret;
+	if(ret!=0){
+		goto stop;
+	}
 
-        // We should /probably/ make sure Range: is supported
+	// We should /probably/ make sure Range: is supported
 	// before we try to do this, but these 8 bytes are the titleId
 	ret = httpcAddRequestHeaderField(&context, (char*)"Range", (char*)"bytes=11292-11299");
-        if(ret!=0)return ret;
+	if(ret!=0){
+		goto stop;
+	}
 
-        // This disables the SSL certificate checks.
-        ret = httpcSetSSLOpt(&context, 1<<9);
-        if(ret!=0)return ret;
-
-	ret = httpcBeginRequest(&context);
-	if(ret!=0)return ret;
-
-	ret = httpcGetResponseStatusCode(&context, &statuscode, 0);
-	if(ret!=0)return ret;
-
-        if(statuscode==301||statuscode==302) {
-                if(httpcGetResponseHeader(&context, (char*)"Location", (char*)url, QUIRC_MAX_PAYLOAD-1)==0){
-                        return http_getinfo(url, app);
-                }       
-        } 
-
-	if(statuscode!=206)return -2; // 206 Partial Content
-
-	u8 *buf = (u8*)malloc(8); // Allocate u8*8 == u64
-	if(buf==NULL)return -1;
-	memset(buf, 0, 8); // Zero out
-
-	ret=httpcDownloadData(&context, buf, 8, NULL);
-        // Safely convert our 8 byte string into a u64
-	app->titleId = ((u64)buf[0] << 56 | (u64)buf[1] << 48 | (u64)buf[2] << 40 | (u64)buf[3] << 32 | (u64)buf[4] << 24 | (u64)buf[5] << 16 | (u64)buf[6] << 8 | (u64)buf[7]);
-	free(buf);
-
-        buf = (u8*)malloc(64);
-
-        if(httpcGetResponseHeader(&context, (char*)"Content-Range", (char*)buf, 64)==0){
-		char *ptr = strchr((const char *)buf, 47);
-		app->size = atoll(&ptr[1]);
-        }
-        free(buf);
-
-
-	ret = httpcCloseContext(&context);
-        if(ret!=0)return ret;
-
-	return 0;
-}
-
-Result http_download(char *url, app::App *app) {
-	Result ret=0;
-	httpcContext context;
-	u32 statuscode=0;
-        u32 contentsize=0, downloadsize=0;
-	char *buf;
-
-	ret = httpcOpenContext(&context, HTTPC_METHOD_GET, url, 0);
-        if(ret!=0)return ret;
-
-        ret = httpcAddRequestHeaderField(&context, (char*)"Accept-Encoding", (char*)"gzip, deflate");
-        if(ret!=0)return ret;
-
-        // This disables the SSL certificate checks.
-        ret = httpcSetSSLOpt(&context, 1<<9);
-        if(ret!=0)return ret;
+	// This disables the SSL certificate checks.
+	ret = httpcSetSSLOpt(&context, SSLOPTION_NOVERIFY);
+	if(ret!=0){
+		goto stop;
+	}
 
 	ret = httpcBeginRequest(&context);
-	if(ret!=0)return ret;
+	if(ret!=0){
+		goto stop;
+	}
 
 	ret = httpcGetResponseStatusCode(&context, &statuscode, 0);
-	if(ret!=0)return ret;
+	if(ret!=0){
+		goto stop;
+	}
 
-	if(statuscode!=200)return -2;
+	if(statuscode==301||statuscode==302){
+		ret = httpcGetResponseHeader(&context, (char*)"Location", (char*)url, QUIRC_MAX_PAYLOAD-1);
+		if(ret!=0){
+			goto stop;
+		}
 
-	ret=httpcGetDownloadSizeState(&context, &downloadsize, &contentsize);
-	if(ret!=0)return ret;
+		ret=http_getinfo(url, app);
+		goto stop;
+	}
 
-        buf = (char*)malloc(16);
-	if(buf==NULL)return -1;
-	memset(buf, 0, 16);
+	if(statuscode==206){
+		buf = (u8*)malloc(8); // Allocate u8*8 == u64
+		if(buf==NULL){
+			goto stop;
+		}
+		memset(buf, 0, 8); // Zero out
 
-	if(httpcGetResponseHeader(&context, (char*)"Content-Encoding", (char*)buf, 16)==0){
-                printf("Content-Encoding: %s\n", buf);
-        }
+		ret = httpcDownloadData(&context, buf, 8, NULL);
 
-        app::install(app->mediaType, &context, app->size, &onProgress);
+		// Safely convert our 8 byte string into a u64
+		app->titleId = ((u64)buf[0]<<56|(u64)buf[1]<<48|(u64)buf[2]<<40|(u64)buf[3]<<32|(u64)buf[4]<<24|(u64)buf[5]<<16|(u64)buf[6]<<8|(u64)buf[7]);
+		free(buf);
 
-	free(buf);
+		buf = (u8*)malloc(64);
+		if(buf==NULL){
+			goto stop;
+		}
 
+		ret = httpcGetResponseHeader(&context, (char*)"Content-Range", (char*)buf, 64);
+		if(ret==0){
+			char *ptr = strchr((const char *)buf, 47);
+			app->size = atoll(&ptr[1]);
+		}
+
+		free(buf);
+	} else {
+		printf("HTTP status: %lu", statuscode);
+	}
+
+	stop:
 	ret = httpcCloseContext(&context);
-	if(ret!=0)return ret;
 
 	return ret;
 }
 
-int main(int argc, char **argv)
-{
+Result http_download(char *url, app::App *app){
+	Result ret=0;
+	httpcContext context;
+	u32 statuscode=0;
+	u32 contentsize=0, downloadsize=0;
+	char *buf;
+
+	ret = httpcOpenContext(&context, HTTPC_METHOD_GET, url, 0);
+	if(ret!=0){
+		goto stop;
+	}
+
+	ret = httpcAddRequestHeaderField(&context, (char*)"Accept-Encoding", (char*)"gzip, deflate");
+	if(ret!=0){
+		goto stop;
+	}
+
+	// This disables the SSL certificate checks.
+	ret = httpcSetSSLOpt(&context, 1<<9);
+	if(ret!=0){
+		goto stop;
+	}
+
+	ret = httpcBeginRequest(&context);
+	if(ret!=0){
+		goto stop;
+	}
+
+	ret = httpcGetResponseStatusCode(&context, &statuscode, 0);
+	if(ret!=0){
+		goto stop;
+	}
+
+	if(statuscode==200){
+		ret = httpcGetDownloadSizeState(&context, &downloadsize, &contentsize);
+		if(ret!=0){
+			goto stop;
+		}
+
+		buf = (char*)malloc(16);
+		if(buf==NULL){
+			goto stop;
+		}
+		memset(buf, 0, 16);
+
+		ret = httpcGetResponseHeader(&context, (char*)"Content-Encoding", (char*)buf, 16);
+		if(ret!=0){
+			goto stop;
+		}
+
+		printf("Content-Encoding: %s\n", buf);
+
+		app::install(app->mediaType, &context, app->size, &onProgress);
+
+		free(buf);
+	} else {
+		printf("HTTP status: %lu", statuscode);
+	}
+
+	stop:
+	ret = httpcCloseContext(&context);
+	printf("ret=%lx\n", ret);
+
+	return ret;
+}
+
+int main(int argc, char **argv){
 	Result ret=0;
 
-        core::init(argc);
+	core::init(argc);
 	httpcInit(0x1000);
 
 	consoleInit(GFX_BOTTOM,NULL);
@@ -144,7 +189,7 @@ int main(int argc, char **argv)
 
 	char *url = (char*)malloc(QUIRC_MAX_PAYLOAD);
 	FILE *fd = fopen(AUTO_UPDATE_FILE, "r");
-	if(fd != NULL) {
+	if(fd != NULL){
 		fread(url, sizeof(char), QUIRC_MAX_PAYLOAD - 1, fd);
 		fclose(fd);
 	} else
@@ -156,7 +201,7 @@ int main(int argc, char **argv)
 	ret = http_getinfo(url, &app);
 	if(ret!=0)return ret;
 
-	if(app.titleId != 0 && app::installed(app)) { // Check if we have a titleId to remove
+	if(app.titleId != 0 && app::installed(app)){ // Check if we have a titleId to remove
 		printf("Uninstalling titleId: 0x%llx\n", app.titleId);
 		gpu::flushBuffer();
 		app::uninstall(app);
